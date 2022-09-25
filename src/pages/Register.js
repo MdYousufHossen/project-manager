1af404cdd136c558052e61d4a548a8ee21a17bcd
MdYousufHossen <yousufadnan111@gmail.com>
2022-09-25 18:36:35 +0600

    add avatar url placeholder

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -138,7 +138,7 @@ if(password!==confirmPassword){
                                     type="url"
                                     required
                                     className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
-                                    placeholder="Url"
+                                    placeholder="Profile Avatar Url."
                                     value={url}
                                     onChange={(e)=>setUrl(e.target.value)}
                                 />
